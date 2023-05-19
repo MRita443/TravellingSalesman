78c0bd3126344f2b64e178af9c6846d840179ad9
Rita Lopes <up202108819@up.pt>
2023-05-19 14:40:07 +0100

    Added tsp backtracking

---
+++ src/graph.h
@@ -0,0 +1,14 @@
+//
+// Created by rita on 19-05-2023.
+//
+
+#ifndef TRAVELLINGSALESMAN_GRAPH_H
+#define TRAVELLINGSALESMAN_GRAPH_H
+
+
+class graph {
+
+};
+
+
+#endif //TRAVELLINGSALESMAN_GRAPH_H
